80fcd93e5ca2d44c81571ce4b363c1504a41b3c5
md muzaffar ali <amuzaffar857@gmail.com>
2015-08-11 16:12:10 +0530

    new files has been added

---
+++ css.php
@@ -0,0 +1,264 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+<style media="screen">
+
+.body {
+    background-image: url("http://7-themes.com/data_images/out/38/6898677-beautiful-beach-background.jpg");
+}
+.body1 {
+    background-image: url("http://orig08.deviantart.net/6ae5/f/2009/136/2/f/windows_7_login_wallpaper_edit_by_flclrockstar.jpg");
+}
+
+.body2 {
+    background-color: #ADBAD4;
+}
+
+.hght{
+height: 100px;
+background: #2f73bc;
+outline: 1px solid black;
+text-align: center;
+font-family: monospace;
+border-radius: 10px;
+}
+.hhh1{
+height:75px;
+
+}
+.hhh2 {
+  height: 85px;
+}
+.hhh3 {
+  height: 75px;
+  border: 2px solid purple;
+  border-radius: 20px;
+
+}
+.hhh4 {
+  margin-bottom: 50px;
+}
+.hhh5 {
+  height: 275px;
+  margin-top: 35px;
+}
+
+.hhh {
+  height: 75px;
+}
+.hght{
+  background: teal;
+  outline: 1px solid black;
+  text-align: center;
+}
+
+.hght1{
+  /*height: 120px;*/
+  background: teal;
+  outline: 1px solid blue;
+}
+
+
+.hght2{
+  height: 100px;
+  background: purple;
+  font-family: monospace;
+  border-radius: 40px;
+  padding-top: 10px;
+  border: 4px solid grey;
+  margin: 30px 0 0 100px ;
+  color: blue;
+  font-family: monospace;
+  box-shadow: 10px 7px 10px 0;
+  text-align: center;
+}
+
+.box-shadow1 {
+  box-shadow: 10px 7px 10px 0;
+}
+.hght3{
+  height: 150px;
+  background: purple;
+  font-family: monospace;
+  border-radius: 40px;
+  padding-top: 25px;
+  border: 4px solid grey;
+  margin: 30px 0 0 30px ;
+  color: blue;
+  font-family: monospace;
+  box-shadow: 10px 7px 10px 0;
+}
+
+.height {
+  height: 100px;
+
+}
+.border {
+  border: 2px solid purple ;
+  padding: 10px 20px 10px 20px;
+  border-radius: 20px;
+  margin: 0 500px 0 0;
+
+}
+
+.border1 {
+  border: 2px solid purple;
+  padding: 10px 20px 10px 20px;
+  border-radius: 20px;
+
+
+}
+.border5 {
+  border: 2px solid purple;
+  padding: 10px 10px 10px 10px;
+  border-radius: 20px;
+  margin-right: 50px;
+  margin-top: 12px;
+}
+
+.border2 {
+  border: 2px solid purple;
+  border-radius: 15px;
+  height:50px;
+  box-shadow: 10px 7px 10px 0;
+
+}
+.border3 {
+  border: 2px solid purple;
+  border-radius: 20px;
+  height:75px;
+  padding: 10px 60px 10px 60px;
+  margin: 50px 75px 0 0;
+  font-size: 25px;
+  color: darkolivegreen;
+
+}
+.border4 {
+  border: 2px solid purple;
+  border-radius: 20px;
+  height:50px;
+  padding: 10px 40px 15px 40px;
+  margin: 12px 180px 0 0;
+  font-size: 25px;
+  color: darkolivegreen;
+  /*margin-right: 200px;*/
+
+}
+.border6 {
+  border: 2px solid purple;
+  border-radius: 20px;
+  height:50px;
+  padding: 10px 40px 15px 40px;
+  margin: 12px 100px 0 0;
+  font-size: 25px;
+  color: darkolivegreen;
+  /*margin-right: 100px;*/
+
+}
+.border7 {
+  margin-right: 50px;
+  margin-top: 25px;
+  display: block;
+}
+.border8 {
+  font-size: 30px;
+  font-family: monospace;
+  font-style: italic;
+  margin: 25px 0 0 25px;
+  padding: 0;
+}
+
+.margin {
+  margin-top: 10px;
+}
+.write {
+ color: purple;
+  font-size: 30px;
+  margin: 50px 50px 0 70px;
+}
+.h3 {
+  height: 150px;
+  border: 5px solid purple;
+}
+.img {
+  height: 75px;
+  width: 100px;
+  margin-top: 8px;
+}
+.img1 {
+  height: 75px;
+  width: 100px;
+  margin-top: 18px;
+  margin-left: 20px;
+}
+.h4 {
+  height: 100px;
+  border: 5px solid purple;
+  border-radius: 30px;
+}
+.write5 {
+
+  font-size: 20px;
+  margin-top: 35px;
+  display: block;
+}
+.write6 {
+  display: block;
+  margin: 15px 0 0 20px;
+  font-size: 20px;
+}
+.h5 {
+  border: 5px solid purple;
+  height: 1000px;
+  border-radius: 15px;
+}
+.h6 {
+  border: 5px solid purple;
+  height: 220px;
+  border-radius: 15px;
+}
+.recent {
+  text-align: center;
+  font-size: 30px;
+  text-decoration: underline;
+  height: 50px;
+  margin-top: 20px;
+}
+.recent1 {
+  text-align: left;
+  font-size: 30px;
+  text-decoration: underline;
+  height: 50px;
+  margin-top: 20px;
+  margin-left: 20px;
+  font-style: italic;
+}
+
+.recent3 {
+  text-align: center;
+  font-size: 20px;
+  text-decoration: underline;
+  height: 50px;
+  margin-top: 20px;
+
+}
+
+.recent4 {
+  text-align: left;
+  font-size: 25px;
+  text-decoration: underline;
+  height: 50px;
+  margin-top: 20px;
+  margin-left: 20px;
+  font-style: italic;
+}
+
+.h10 {
+  height: 1200px;
+  /*border: 1px solid purple;*/
+
+}
+
+</style>
